1246d92182a7d4b0fb03bac385c9232e0e5efe38
iago <iagobem2010@gmail.com>
2023-10-23 09:24:48 -0300

    bugfix

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var downloadButton = document.getElementById('downloadButton');
+var downloadButton = document.getElementById("downloadButton");
 
-downloadButton.addEventListener('click', function() {
-  var pdfURL = './assets/downloads/cv.pdf';
+downloadButton.addEventListener("click", function () {
+  var pdfURL = "./assets/downloads/cv.pdf";
 
-  var link = document.createElement('a');
+  var link = document.createElement("a");
   link.href = pdfURL;
-  link.download = 'Iago-Jorge-CV.pdf';
+  link.download = "Iago-Jorge-CV.pdf";
 
   link.click();
 });
@@ -14,16 +14,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const imageHoverElements = document.querySelectorAll(".skills");
 
   imageHoverElements.forEach((element) => {
-      element.addEventListener("mouseover", function () {
-          const text = element.getAttribute("data-text");
-          const titulo = element.getAttribute("data-titulo");
-          const info = element.parentElement.nextElementSibling.querySelector(".info");
-          info.innerHTML = `<h1 class="title">${titulo}</h1><h2 class="subtitle">${text}</h2>`;
-      });
+    element.addEventListener("mouseover", function () {
+      const text = element.getAttribute("data-text");
+      const titulo = element.getAttribute("data-titulo");
+      const info =
+        element.parentElement.nextElementSibling.querySelector(".info");
+      info.innerHTML = `<h1 class="title">${titulo}</h1><h2 class="subtitle">${text}</h2>`;
+    });
 
-      element.addEventListener("mouseout", function () {
-        const info = element.parentElement.nextElementSibling.querySelector(".info");
-        info.innerHTML = `<h1 class="title">Habilidades</h1><h2 class="subtitle">Aponte para uma skill para saber mais!</h2>`;
+    element.addEventListener("mouseout", function () {
+      const info =
+        element.parentElement.nextElementSibling.querySelector(".info");
+      info.innerHTML = `<h1 class="title">Habilidades</h1><h2 class="subtitle">Aponte para uma skill para saber mais!</h2>`;
     });
   });
 });
@@ -36,7 +38,6 @@ timeTracker.addEventListener("click", function () {
 
 var newProject = document.getElementById("new-project");
 
-newProject.addEventListener("click", function(){
+newProject.addEventListener("click", function () {
   window.open("https://", "_blank");
-})
-
+});
